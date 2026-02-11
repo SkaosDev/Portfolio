63ab419d2e29f9ce52df9cdb4253ffaed149c53c
SkaosDev <skaos@mythik.fr>
2026-02-11 20:56:41 +0100

    Change color and image

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
                     <div className="absolute bottom-6 sm:bottom-8 md:bottom-10">
                         <div className="draw flex mb-5">
                             <div className="demi-cercle bg-orange-custom"></div>
-                            <div className="demi-cercle bg-yellow-custom"></div>
+                            <div className="demi-cercle bg-pink-custom"></div>
                             <div className="cercle bg-black"></div>
                         </div>
                         <h2 className="text-6xl sm:text-7xl md:text-8xl font-normal md:font-light">clément royer.</h2>
@@ -22,7 +22,7 @@ function App() {
                 <section id="citation" className="flex justify-center items-end flex-col text-right">
                     <div className="relative font-extralight text-[22px] sm:text-3xl md:text-4xl max-w-5/6 md:max-w-3/4 lg:max-w-2/3 leading-none">
                         <div className="absolute left-0 top-1/2 -translate-y-1/2 -translate-x-1/4 z-0">
-                            <div className="big-cercle bg-yellow-custom"></div>
+                            <div className="big-cercle bg-pink-custom"></div>
                         </div>
                         <span className="relative z-2">
                             "the people who are crazy enough to think they can change the world, are the ones who do."
